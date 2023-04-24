5c7cafa4398f6be2666b2058cb73cb4e8c20208e
Geoff <gsweetwood123@gmail.com>
2023-04-24 11:16:38 -0400

    Added button to set size of grid and added mouseover event listener to change divs to black

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // set variables
-let gridSize = 16;
+let gridSize;
 const mainDiv = document.querySelector(`#main`);
+const btn = document.querySelector(`button`);
 
 
 //loop to set put grid in div of html
-for (i=0; i <= gridSize; i++) {
-    const squareDiv = document.createElement(`div`);
-    mainDiv.appendChild(squareDiv);
+
+
+btn.addEventListener(`click`, ()=> {
+    gridSize = prompt(`Enter grid size up to 100: `);
+    if (gridSize > 100) {
+        gridSize = prompt(`Please enter a number up to 100: `);
+    }
+    setGrid();
+});
+
+const setGrid = () => {
+    for (i=0; i < gridSize; i++) {
+        const squareDiv = document.createElement(`div`);
+        mainDiv.appendChild(squareDiv);
+        squareDiv.classList.add(`sketch`);
+        squareDiv.addEventListener(`mouseover`, ()=> {
+            squareDiv.classList.add(`sketch-over`);
+        })
+    }
 }
